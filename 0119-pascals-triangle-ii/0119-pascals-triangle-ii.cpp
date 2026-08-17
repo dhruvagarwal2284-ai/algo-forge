3f8d8eca8e61be4f10e1f9d2072094c75dc0b91d
dhruvagarwal2284-ai <dhruvagarwal2284@gmail.com>
2026-08-17 14:43:53 +0530

    Time: 0 ms (100%), Space: 9 MB (50.7%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        int a=0;
+        vector<int> vec;
+        while(a!=rowIndex){
+            vec.push_back(1);
+            int i=vec.size()-1;
+            while(i>0){
+                vec[i]=vec[i]+vec[i-1];
+                i--;
+            }
+            a++;
+        }
+        vec.insert(vec.end(),1);
+        return vec;
+    }
+};
